16a3c2c33739a55c1b8e8e0a97e2a552cbf59a14
Dylan Smith <dylan@rh.izo.me>
2016-02-11 23:11:04 -0800

    wip

--- .zshrc.d/ls_custom.zsh
+++ .zshrc.d/ls_custom.zsh
@@ -1,3 +1,21 @@
 function lll() {
-  ls -AlGp | awk '{printf("%s\t%4s\t%s\t%s\n", $1, $2, $3, $4)}'
+  export Green=$'\x1b[38;2;150;180;90m'
+  export GreenDark=$'\x1b[38;2;130;150;90m'
+  export Italic=$'\e[3m'
+  export Red=$'\e[0;31m'
+  export Plain=$'\e[0m'
+
+  ls -Alp | awk '$9 ~ /^\..+[^/]$/ {
+    printf(ENVIRON["Italic"]ENVIRON["Green"]"%-14s%s ∴ %s  %s\n"ENVIRON["Plain"], $1, $3, $4, $9)
+  }
+  $9 ~ /^\..+\/$/ {
+    printf(ENVIRON["Italic"]ENVIRON["GreenDark"]"%-14s%s ∴ %s  %s\n"ENVIRON["Plain"], $1, $3, $4, $9)
+  }'
+
+  # ls -Alp | awk '$9 ~ /^\./ {
+    # printf(ENVIRON["Italic"]ENVIRON["Green"]"%-14s%s ∴ %s  %s\n"ENVIRON["Plain"], $1, $3, $4, $9)
+  # }
+  # {print "poop"}'
+
+  # awk 'BEGIN { print "Output: " ENVIRON["Italic"] ENVIRON["Green"] "Hello" ENVIRON["Color_Off"] " Bye" }'
 }
