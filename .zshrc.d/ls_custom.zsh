6efd3e9c15a0643b1d761a1e116bb98ffc457d6c
Dylan Smith <dylan@rh.izo.me>
2016-02-16 09:02:34 -0800

    wip

--- .zshrc.d/ls_custom.zsh
+++ .zshrc.d/ls_custom.zsh
@@ -1,5 +1,5 @@
-function ll() {
-  export Bone=$'\x1b[38;2;183;196;170m'
+function ll {
+  export Bone=$'\x1b[38;2;180;192;170m'
   export Blue=$'\x1b[38;2;81;157;234m'
   export BrightBone=$'\x1b[38;2;193;204;180m'
   export BrightBlue=$'\x1b[38;2;88;171;254m'
@@ -13,24 +13,30 @@ function ll() {
   export Italic=$'\e[3m'
   export Plain=$'\e[0m'
 
+  if [[ $# -eq 1 ]]; then
+    export target="`pwd`/$1"
+  else
+    export target=`pwd`
+  fi
+
   # dotfiles
-  ls -Alp | awk '$9 ~ /^\..+[^/]$/ {
-    printf(ENVIRON["Bone"]"%-13s%s %s", $1, $3, $4)
+  ls -Alp $target | awk '$9 ~ /^\..+[^/]$/ {
+    printf(ENVIRON["Bone"]"%-13s%4s  %s %s", $1, $2, $3, $4)
     $1=$2=$3=$4=$5=$6=$7=$8=""; printf(ENVIRON["Italic"]ENVIRON["BrightBone"]"%s\n"ENVIRON["Plain"], $0)
   }
   # dotfile directories
   $9 ~ /^\..+\/$/ {
-    printf(ENVIRON["Green"]"%-13s%s %s", $1, $3, $4)
+    printf(ENVIRON["Green"]"%-13s%4s  %s %s", $1, $2, $3, $4)
     $1=$2=$3=$4=$5=$6=$7=$8=""; printf(ENVIRON["Italic"]ENVIRON["BrightGreen"]"%s\n"ENVIRON["Plain"], $0)
   }
   # regular directories
   $9 ~ /^[^.].+\/$/ {
-    printf(ENVIRON["Magenta"]"%-13s%s %s", $1, $3, $4)
+    printf(ENVIRON["Magenta"]"%-13s%4s  %s %s", $1, $2, $3, $4)
     $1=$2=$3=$4=$5=$6=$7=$8=""; printf(ENVIRON["BrightMagenta"]"%s\n"ENVIRON["Plain"], $0)
   }
   # regular files
-  $9 ~ /^[^.](\w|\W)+[^/]$/ {
-    printf(ENVIRON["Blue"]"%-13s%s %s", $1, $3, $4)
+  $9 ~ /^[^.](\W?|\w?)+[^/]$/ {
+    printf(ENVIRON["Blue"]"%-13s%4s  %s %s", $1, $2, $3, $4)
     $1=$2=$3=$4=$5=$6=$7=$8=""; printf(ENVIRON["BrightBlue"]"%s\n"ENVIRON["Plain"], $0)
   }'
 }
