2da0e48a05e92a813db81a5db8341f4f091b614e
Dylan Smith <dylan@theprogrampdx.com>
2018-01-28 09:31:37 -0800

    Moved aliases away from Dropbox.

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -33,7 +33,7 @@ alias d='cd ~/Desktop'
 alias ddo="doctl"
 alias ddoc="doctl compute"
 alias ddocd="doctl compute droplet"
-alias dev='cd ~/dev'
+alias dev='cd ~/Dev'
 alias dgo="docker run --rm -v /Users/dylan/dev/go:/home/app/go dylansm/golang:1.8.1"
 alias dk='docker'
 alias dkat="docker attach"
@@ -60,7 +60,7 @@ alias dkvrd="docker volume rm $(docker volume ls -f dangling=true -q)"
 # alias dkrms="docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs docker rm"
 # alias dkrs="docker ps -aq --no-trunc | xargs docker rm"
 alias dnsup="docker run -d --name dnsmasq --restart always -p 53535:53/tcp -p 53535:53/udp --cap-add NET_ADMIN andyshinn/dnsmasq --address=/dev/127.0.0.1"
-alias doc='cd ~/Dropbox/Documents'
+alias doc='cd ~/Documents'
 alias dock='cd ~/dev/Docker'
 alias down="cd ~/Downloads"
 alias dsites="cd ~/Dropbox/Sites"
@@ -73,7 +73,7 @@ alias gitref="git reflog --date=iso"
 alias gitu='git add -u'
 alias gitv="git remote -v"
 alias gp="cd /Users/dylan/dev/go/bin/src"
-alias gr='cd ~/dev/GitRepos'
+alias gr='cd ~/Dev/GitRepos'
 alias h="heroku"
 alias history='history 0'
 alias ipc="curl ifconfig.me | pbcopy"
@@ -89,18 +89,17 @@ alias mpcg='mpc listall | grep -Ei '
 alias mpcp="mpc playlist"
 alias mpct="mpc toggle"
 alias mpcu="mpc update"
-alias n='cd ~/Dropbox/Documents/Notes'
-alias notes="cd ~/Dropbox/Documents/Notes"
+alias notes="cd ~/Documents/Notes"
 alias npmg='npm list -g --depth=0 2>/dev/null'
 alias npmgo="npm outdated -g --depth=0"
 alias npml="npm list --depth=0"
 alias npmlo="npm outdated --depth=0"
 alias open="open ."
-alias p='cd ~/dev/Projects'
+alias p='cd ~/Dev/Projects'
 alias pdoc='cd ~/Dropbox/Documents/Project\ Documents'
 alias precomp="RAILS_ENV=production bundle exec rake assets:precompile"
 alias proj="cd ~/Documents/Documents/Project\ Documents/"
-alias proto="cd ~/dev/prototypes"
+alias proto="cd ~/Dev/prototypes"
 alias quote="cd /Users/dylan/Dropbox/Documents/Quotes"
 alias rake='noglob rake' # allows rake and zsh to get along
 alias rakem="bundle exec rake db:migrate"
@@ -108,7 +107,7 @@ alias rakemt="bundle exec rake db:migrate RAILS_ENV=test"
 alias raker="bundle exec rake db:rollback"
 alias rakert="bundle exec rake db:rollback RAILS_ENV=test"
 alias redis28-server="redis28-server /usr/local/etc/redis28.conf"
-alias res='cd ~/Dropbox/Documents/Resume'
+alias res='cd ~/Documents/Resume'
 alias rmorig="find . -name \"*.orig\" -exec rm '{}' ';'"
 alias salias="source ~/.zshrc.d/aliases.zsh"
 alias secret="ruby -rsecurerandom -e 'print SecureRandom.hex(32)' | pbcopy; echo '32-byte hex secret copied to clipboard!'"
@@ -119,7 +118,7 @@ alias spotclear="ssh -t vagrant@10.12.13.18 \"rm -rf /home/vagrant/nike-spot-2.0
 alias spotfresh="spotnewdb && spotclear"
 alias spotnewdb="ssh -t vagrant@10.12.13.18 \"mysql -uroot -psecret -e 'drop database if exists nikespot; create database nikespot;' && cd /home/vagrant/nike-spot-2.0 && php artisan migrate --seed --force\""
 alias ssh='TERM=xterm-256color ssh'
-alias todo='cd ~/Dropbox/Todo'
+alias todo='cd ~/Documents/Todo'
 alias tree2="tree -L 2"
 alias tree3="tree -L 3"
 alias tree4="tree -L 4"
@@ -133,7 +132,7 @@ alias vagst="vagrant global-status"
 alias vagup="vagrant up"
 alias vim="nvim"
 alias vimrc="vim ~/.vimrc"
-alias xcode="cd ~/dev/Xcode/Projects"
+alias xcode="cd ~/Dev/Xcode/Projects"
 alias xplug="cd ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins"
 alias xprefs="find ~/Library/Developer/CoreSimulator/Devices -type f -name "
 alias yarng="yarn global ls"
