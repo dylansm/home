937973662a171c540456bedf26c13fdb2e511c49
Dylan Smith <dylan@theprogrampdx.com>
2019-01-15 15:25:54 -0800

    Removed some old dotfiles

--- .mongorc.js
+++
@@ -1,6 +0,0 @@
-// {/* var host = db.serverStatus().host; */}
-// {/* var prompt = function() { return db+"@"+host+"> "; }; */}
-
-// {/* var prompt = function() { */}
-  // {/* return "Uptime:"+db.serverStatus().uptime+" Documents:"+db.stats().objects+" > "; */}
-// {/* }; */}
