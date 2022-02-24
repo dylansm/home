a03faddd14be8bcf6244261b3d2fdd08f702e633
Dylan Smith <rh@izo.me>
2022-02-23 22:33:59 -0800

    Added name to container.

--- bin/linter_wrappers/eslint.sh
+++ bin/linter_wrappers/eslint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # exec docker run -i --rm -v $(pwd):/data dylansm/eslint -c /data/.eslintrc "$@"
-exec docker run -i --rm -v $(pwd):/data dylansm/eslint-react -c /data/.eslintrc "$@"
+exec docker run -i --rm --name vim-ale -v $(pwd):/data dylansm/eslint-react -c /data/.eslintrc "$@"
