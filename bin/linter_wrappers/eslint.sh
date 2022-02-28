ee8cc59c2ff7e19c26b647c27022f20a4cf9e8d7
Dylan Smith <rh@izo.me>
2022-02-27 21:52:31 -0800

    Updated eslint wrapper to deal with multiple calls more gracefully.

--- bin/linter_wrappers/eslint.sh
+++ bin/linter_wrappers/eslint.sh
@@ -2,20 +2,23 @@
 
 CONTAINER_NAME="vim-ale-eslint"
 
-# [ ! "$(docker ps -a | ${CONTAINER_NAME})" ] && exec # ...
-
 RUNNING=$(docker inspect --format="{{.State.Running}}" ${CONTAINER_NAME} 2> /dev/null)
 
+# if not running run the linter
 if [ ! "${RUNNING}" ]; then
-    # echo "it is running"
-        # # cleanup if stuck
-        # echo "I see ${CONTAINER_NAME}"
-    # fi
-
     exec docker run -i --rm --name "${CONTAINER_NAME}" -v $(pwd):/data dylansm/eslint-react -c /data/.eslintrc "$@"
+
+# otherwise check the state - remove it if frozen; stop and remove if running and run again
 else
+  echo "Already running ${CONTAINER_NAME}. Checking if frozen...";
   if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
-    docker rm "${CONTAINER_NAME}"
+    echo "FROZEN STATE DETECTED... Removing ${CONTAINER_NAME}";
+    docker rm $(docker ps -a -f name="${CONTAINER_NAME}" -q)
+    echo "${CONTAINER_NAME} stopped."
+  else
+    docker stop $(docker ps -a -f name="${CONTAINER_NAME}" -q)
+    docker rm $(docker ps -a -f name="${CONTAINER_NAME}" -q)
+
     exec docker run -i --rm --name "${CONTAINER_NAME}" -v $(pwd):/data dylansm/eslint-react -c /data/.eslintrc "$@"
   fi
 fi
