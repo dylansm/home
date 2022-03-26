f7659206975cbc452bb700df8f95b285c2a41d01
Dylan Smith <rh@izo.me>
2022-03-26 12:22:58 -0700

    Fixed up nvim.zsh

--- .zshrc.d/nvim.zsh
+++ .zshrc.d/nvim.zsh
@@ -1,16 +1,18 @@
 # need to figure out:
 # - copy/paste
 # - MRU
-# - increment Docker container name
 nvim() {
+  # increment container name if already running nvim instances
+  nvim_count=$(( $(docker ps -a --filter="name=nvim*" | wc -l) - 1))
+  [ "$nvim_count" -eq 0 ] && nvim_count=''
+
   if [[ $# -eq 0 ]]; then
-    docker run --rm -it --name nvim -v $(pwd):/home/neovim/workdir dylansm/neovim nvim
+    docker run --rm -it --name "nvim${nvim_count}" -v $(pwd):/home/neovim/workdir dylansm/neovim nvim
   else
     if [[ -d "$1" ]]; then
       dir="$1"
       file="$1"
     else
-      echo "here"
       dir=$(echo "$1" | sed 's|\(.*\)/.*|\1|')
       file=$(echo "$1" | sed 's:.*/::')
     fi
@@ -19,18 +21,11 @@ nvim() {
       dir=$(pwd)
     fi
 
-    # if [[ "$dir" == [a-zA-Z]* ]]; then
-      # echo "dir with no path"
-    # fi
-
     if [[ "$dir" == .* ]] || [[ "$dir" == [a-zA-Z]* ]]; then
-      echo "hidden or no full path provided"
       dir="$(pwd)/$dir"
     fi
-
     # echo "dir=$dir"
     # echo "file=$file"
-
-    docker run --rm -it --name nvim -v $(echo $dir):/home/neovim/workdir dylansm/neovim nvim "$file"
+    docker run --rm -it --name "nvim${nvim_count}" -v $(echo $dir):/home/neovim/workdir dylansm/neovim nvim "$file"
   fi
 }
